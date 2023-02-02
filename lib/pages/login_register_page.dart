c1f6983e9be56ed3fac108b037af99b23218c649
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-02 15:11:24 -0300

    Projeto finalizado

--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -16,8 +16,83 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+          email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+          email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Widget _title() {
+    return Text('Firebase auth');
+  }
+
+  Widget _entryField(String title, TextEditingController controller) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(labelText: title),
+    );
+  }
+
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Humm ? ${errorMessage}');
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+        onPressed: isLoged
+            ? signInWithEmailAndPassword
+            : createUserWithEmailAndPassword,
+        child: Text(isLoged ? 'Login' : 'Register'));
+  }
+
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+        onPressed: () {
+          setState(() {
+            isLoged = !isLoged;
+          });
+        },
+        child: Text(isLoged ? 'Register instead' : 'Login instead'));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _entryField('Email', _controllerEmail),
+            _entryField('Password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _loginOrRegisterButton()
+          ],
+        ),
+      ),
+    );
   }
 }
